e8bc26e9c8e7ef04a5a262284494596eec15e0a7
mariano7717 <mariano7717@gmail.com>
2021-03-14 19:59:23 +0000

    basic random password generator created

--- Passginator/Source.cpp
+++ Passginator/Source.cpp
@@ -1,6 +1,45 @@
+/* ----------------------------------- HEADER ----------------------------------- */
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+
+
+/* ----------------------------------- MAIN ----------------------------------- */
 int main()
 {
-	std::cout << "Hello World!\n";
+	//functions
+	void intro();
+	void randomizer();
+
+	//queue
+	intro();
+	randomizer();
+}
+
+
+
+/* ----------------------------------- FUNCTIONS ----------------------------------- */
+void intro()
+{
+	cout << "\n";
+	cout << "  Welcome to Passginator!\n";
+	cout << "\n";
+	cout << "  If you want a random password, please press R\n";
+	cout << "\n";
+}
+
+void randomizer()
+{
+	//pass parts
+	string passpart1[6] = {"Spring","Summer","Autumn","Winter","Rainy","Sunny"};
+	string passpart2[6] = {"Spring","Summer","Autumn","Winter","Times","Season"};
+	int passpart3 = rand() % 99 + 10;
+
+	int randpasspart1 = rand() % 6;
+	int randpasspart2 = rand() % 6;
+
+	cout << passpart1[randpasspart1];
+	cout << passpart2[randpasspart2];
+	cout << passpart3;
 }
