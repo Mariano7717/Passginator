05200825a4bcb344ace957488daf876aa1d5d31e
mariano7717 <mariano7717@gmail.com>
2021-03-15 22:36:49 +0000

    Conditions added, pending test

--- Passginator/Source.cpp
+++ Passginator/Source.cpp
@@ -1,11 +1,12 @@
-/* ----------------------------------- HEADER ----------------------------------- */
+/* ------------------------------------------------ HEADER ------------------------------------------------ */
 #include <iostream>
 #include <stdlib.h>
+#include <Windows.h>
 using namespace std;
 
 
 
-/* ----------------------------------- MAIN ----------------------------------- */
+/* ------------------------------------------------ MAIN ------------------------------------------------ */
 int main()
 {
 	//functions
@@ -14,18 +15,76 @@ int main()
 
 	//queue
 	intro();
-	randomizer();
+
+	
+	///* Woorks....IF
+	//Initialize and collect keyboard variable
+	string keypress;
+	cin >> keypress;
+	if (keypress == "R" )
+	{
+		cout << "inside if\n";
+		randomizer();
+		cout << "\n";
+	}
+	else if (keypress == "r" )
+	{
+		cout << "inside elseif\n";
+		randomizer();
+		cout << "\n";
+	}
+	else if (keypress == "q" )
+	{
+		cout << "inside elseif\n";
+		randomizer();
+		cout << "\n";
+	}
+	else if (keypress == "Q" )
+	{
+		cout << "inside elseif\n";
+		randomizer();
+		cout << "\n";
+	}
+	else
+	{
+		cout << "Start again\n\n";
+		
+	}
+	//*/
+	
+	
+	/*
+	* Doesnt work
+	* 
+	if (GetKeyState('R') & 0x8000)
+	//Check if high-order bit is set (1 << 15)
+
+	{
+		randomizer();
+	}
+	*/
+
+	//to be reviewed
+	/*switch (2) {
+		case keypress == R :
+			randomizer();
+		case keypress == M :
+			;
+		default:
+			break;
+	}*/
+
 }
 
 
 
-/* ----------------------------------- FUNCTIONS ----------------------------------- */
+/* ------------------------------------------------ FUNCTIONS ------------------------------------------------ */
 void intro()
 {
 	cout << "\n";
 	cout << "  Welcome to Passginator!\n";
 	cout << "\n";
-	cout << "  If you want a random password, please press R\n";
+	cout << "  If you want a random password, press [R]\n";
 	cout << "\n";
 }
 
@@ -39,6 +98,7 @@ void randomizer()
 	int randpasspart1 = rand() % 6;
 	int randpasspart2 = rand() % 6;
 
+	cout << "This is the newly generated password!!:\n";
 	cout << passpart1[randpasspart1];
 	cout << passpart2[randpasspart2];
 	cout << passpart3;
